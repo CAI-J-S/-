e87d49ab7d97dfe989eb479d234814cfa2711960
CAI-J-S <1065458078@qq.com>
2023-12-13 09:59:05 +0800

    图片长廊优化

--- scripts/index.js
+++ scripts/index.js
@@ -10,14 +10,56 @@ let 当前索引 = 初始索引;
 const 初始位移 = 图片宽度 / 2;
 const 移动时长 = 250;
 
-左箭头.addEventListener("click", 向前移动);
-右箭头.addEventListener("click", 向后移动);
+const 背景图 = document.querySelector(".背景图");
+const 背景图路径 = "./图片/index/";
+背景图.src = `${背景图路径}3.webp`;
+
+const 自动播放间隔 = 3000;
+let 自动播放 = setInterval(向后移动, 自动播放间隔);
+
+左箭头.addEventListener("click", () => {
+  clearInterval(自动播放);
+  setTimeout(() => {
+    自动播放 = setInterval(向后移动, 自动播放间隔);
+  }, 移动时长);
+  向前移动();
+});
+
+右箭头.addEventListener("click", () => {
+  clearInterval(自动播放);
+  setTimeout(() => {
+    自动播放 = setInterval(向后移动, 自动播放间隔);
+  }, 移动时长);
+  向后移动();
+});
 
 function 向后移动() {
   图片长廊.style.transition = `${移动时长}ms`;
   当前索引++;
   图片长廊.style.translate = `${初始位移 - (当前索引 - 初始索引) * 图片宽度}px`;
   右箭头.style.pointerEvents = "none";
+  switch (当前索引) {
+    case 0:
+    case 4:
+    case 8:
+      背景图.src = `${背景图路径}2.webp`;
+      break;
+    case 1:
+    case 5:
+    case 9:
+      背景图.src = `${背景图路径}3.webp`;
+      break;
+    case 2:
+    case 6:
+    case 10:
+      背景图.src = `${背景图路径}4.webp`;
+      break;
+    case 3:
+    case 7:
+    case 11:
+      背景图.src = `${背景图路径}5.webp`;
+      break;
+  }
   setTimeout(() => {
     右箭头.style.pointerEvents = "all";
     if (当前索引 === 初始索引 + 图片数量) {
@@ -31,8 +73,30 @@ function 向后移动() {
 function 向前移动() {
   图片长廊.style.transition = `${移动时长}ms`;
   当前索引--;
-  图片长廊.style.translate = `${初始位移 + (当前索引 - 初始索引) * 图片宽度}px`;
+  图片长廊.style.translate = `${初始位移 - (当前索引 - 初始索引) * 图片宽度}px`;
   左箭头.style.pointerEvents = "none";
+  switch (当前索引) {
+    case 0:
+    case 4:
+    case 8:
+      背景图.src = `${背景图路径}2.webp`;
+      break;
+    case 1:
+    case 5:
+    case 9:
+      背景图.src = `${背景图路径}3.webp`;
+      break;
+    case 2:
+    case 6:
+    case 10:
+      背景图.src = `${背景图路径}4.webp`;
+      break;
+    case 3:
+    case 7:
+    case 11:
+      背景图.src = `${背景图路径}5.webp`;
+      break;
+  }
   setTimeout(() => {
     左箭头.style.pointerEvents = "all";
     if (当前索引 === 初始索引 - 图片数量) {
